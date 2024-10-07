ed45aed4bbebb8974111324cf686fda34bda5b66
Abioye Ubaidah <159426073+RayoPotter@users.noreply.github.com>
2024-10-07 14:07:45 +0100

    family page

--- js/family.js
+++ js/family.js
@@ -1,3 +1,13 @@
+document.getElementById('menuIcon').addEventListener('click', function() {
+    var menuItems = document.getElementById('menuItems');
+    if (menuItems.classList.contains('active')) {
+        menuItems.classList.remove('active');
+    } else {
+        menuItems.classList.add('active');
+    }
+});
+
+
 function showModal(text) {
     document.getElementById("modal-text").innerText = text;
     document.getElementById("modal").style.display = "block";
